4d8d465487714b2a25353999dc571f2eba0eb1ba
jalalisika <jalal.q@hotmail.fr>
2024-10-31 09:18:50 +0100

    Creation Hello World

---
+++ src/main/java/fr/isika/cda28git/fr/isika/cda28git/HelloWord.java
@@ -0,0 +1,12 @@
+package fr.isika.cda28git.fr.isika.cda28git;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
